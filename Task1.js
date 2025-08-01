aca14becec42554fdd9f063ec89b9128d8010f86
Fahim <fahim1020pantho@gmail.com>
2025-08-01 23:08:25 +0600

    task1 is added

---
+++ Task1.js
@@ -0,0 +1,8 @@
+let taka = 1000;
+let applePrice = 300;
+let appleQuantity = 2;
+let orangePrice = 80;
+let orangeQuantity = 3;
+let totalCost = (applePrice * appleQuantity) + (orangePrice * orangeQuantity);
+let remainTaka = taka - totalCost;
+console.log(`Remain Taka: ${remainTaka}`);
